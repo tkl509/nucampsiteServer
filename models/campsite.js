346da6f7862303a99c9ceb95de21da268c7ef640
Tami Lankford <tklankford@gmail.com>
2020-02-11 23:16:50 -0600

    Mongoose Part 3

--- models/campsite.js
+++ models/campsite.js
@@ -14,6 +14,10 @@ const commentSchema = new Schema({
     text: {
         type: String,
         required: true
+    },
+    author: {
+        type: String,
+        required: true
     }
 }, {
     timestamps: true
