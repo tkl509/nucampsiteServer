a5ebfbd4909a725f5d81afafb3b80ec5f6669c3d
tkl509 <tklankford@aol.com>
2020-02-26 19:03:15 -0600

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '565020630752120',
-        clientSecret: '9d3a6aec5ff0b95d7c231ba3b499799c'
-    }
-}
